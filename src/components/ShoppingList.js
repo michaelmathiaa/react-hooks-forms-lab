c77e17142ea3d412cbfefdafdc66f769e3f04aec
Michael Mathias <michaelscodingjourney@gmail.com>
2023-06-12 17:18:46 -0700

    Passed eight tests for react hooks forms lab

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -5,12 +5,24 @@ import Item from "./Item";
 
 function ShoppingList({ items }) {
   const [selectedCategory, setSelectedCategory] = useState("All");
+  const [search, setSearch] = useState('');
+  const [newItems, setNewItems] = useState(items);
 
   function handleCategoryChange(event) {
     setSelectedCategory(event.target.value);
   }
 
-  const itemsToDisplay = items.filter((item) => {
+  function handleSearchChange(event) {
+    setSearch(event.target.value);
+  }
+
+  function handleItemFormSubmit(newItem) {
+    setNewItems([...newItems, newItem]);
+  }
+
+  const itemsToDisplay = newItems.filter((item) => {
+    if (search !== "") return item.name.includes(search);
+
     if (selectedCategory === "All") return true;
 
     return item.category === selectedCategory;
@@ -18,8 +30,8 @@ function ShoppingList({ items }) {
 
   return (
     <div className="ShoppingList">
-      <ItemForm />
-      <Filter onCategoryChange={handleCategoryChange} />
+      <ItemForm onItemFormSubmit={handleItemFormSubmit} />
+      <Filter onCategoryChange={handleCategoryChange} onSearchChange={handleSearchChange} />
       <ul className="Items">
         {itemsToDisplay.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
